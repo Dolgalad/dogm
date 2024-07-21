996e3eeeef51bfd32fa6453b781e20d432bb729c
Dolgalad <a.schulz1992@gmail.com>
2024-07-21 15:40:01 +0200

    lazy loading

--- mis_train_lazy_nocopy.cpp
+++ mis_train_lazy_nocopy.cpp
@@ -8,6 +8,7 @@
 #include<chrono>
 #include<thread>
 #include<utility>
+#include<mutex>
 
 #include<torch/torch.h>
 
@@ -295,22 +296,101 @@ struct MISGNNData {
 		return g;
 	};
 
+	static shared_ptr<struct MISGNNData> from_cache(path cached_path) {
+		string p = cached_path.string();
+		vector<Tensor> data;
+		torch::load(data, p);
+		shared_ptr<struct MISGNNData> g = make_shared<struct MISGNNData>();
+		g->edge_index = data[0];
+		g->x = data[1];
+		g->y = data[2];
+		g->y_ind = data[3];
+		g->var_mask = data[4];
+		g->var_marginal_mask = data[5];
+		g->bp_edges->r_edges = data[6];
+		g->bp_edges->q_edges = data[7];
+		g->bp_edges->f2v_edges = data[8];
+		g->bp_edges->m_edges = data[9];
+		g->bp_edges->vm_edges = data[10];
+		g->bp_edges->num_vars = data[11];
+		g->bp_edges->num_ss = data[12];
+		g->bp_edges->num_fact = data[13];
+		g->bp_edges->num_f2v_msg = data[14];
+		g->bp_edges->m_stride = data[15];
+		g->bp_edges->ss_index = data[16];
+		g->bp_edges->v_ss_index_mask = data[17];
+		g->n = data[11].item<int>();
+		g->m = data[13].item<int>() - g->n;
+		g->fg = make_shared<struct FactorGraph>();
+		g->n_batch = 1;
+
+		return g;
+
+
+	};
+
+	void save_to_cache(path cached_path) {
+		vector<Tensor> data;
+		data.push_back(edge_index);
+		data.push_back(x);
+		data.push_back(y);
+		data.push_back(y_ind);
+		data.push_back(var_mask);
+		data.push_back(var_marginal_mask);
+		data.push_back(bp_edges->r_edges);
+		data.push_back(bp_edges->q_edges);
+		data.push_back(bp_edges->f2v_edges);
+		data.push_back(bp_edges->m_edges);
+		data.push_back(bp_edges->vm_edges);
+		data.push_back(bp_edges->num_vars);
+		data.push_back(bp_edges->num_ss);
+		data.push_back(bp_edges->num_fact);
+		data.push_back(bp_edges->num_f2v_msg);
+		data.push_back(bp_edges->m_stride);
+		data.push_back(bp_edges->ss_index);
+		data.push_back(bp_edges->v_ss_index_mask);
+		torch::save(data, cached_path.string());
+	};
+
 
 };
 
-using Data = std::vector<pair<shared_ptr<MISGNNData>, torch::Tensor> >;
+using Data = std::vector<path >;
 using MISGNNExample = torch::data::Example<shared_ptr<MISGNNData>, torch::Tensor>;
+
+std::mutex rw_guard;
 
 class MISGNNDataset : public torch::data::datasets::Dataset<MISGNNDataset, MISGNNExample > {
 	using Example = MISGNNExample;
 	using Batch = vector<Example>;
 	using BatchRequest = c10::ArrayRef<size_t>;
-	const vector<pair<shared_ptr<MISGNNData>, torch::Tensor> > data;
+	const vector<path> data;
+	const path cache_dir;
+	const torch::Device device;
 
 	public:
-		MISGNNDataset(const Data& data) : data(data) {}
+		MISGNNDataset(const Data& data, const path cache_dir, const torch::Device& device) : data(data), cache_dir(cache_dir), device(device) {}
 		Example get(size_t index) {
-			return Example(data[index].first, data[index].second);
+			// check if example is available in cache director
+			path cached_example_path = cache_dir / data[index].filename();
+			if(exists(cached_example_path)) {
+				rw_guard.lock();
+				std::shared_ptr<MISGNNData> mis = MISGNNData::from_cache(cached_example_path);
+				rw_guard.unlock();
+				return Example(mis, mis->y);
+
+			} else {
+				path graph_path = data[index];
+				path feature_path = replace_all(graph_path.string(),".graph", "_features.csv");
+				path solution_path = replace_all(graph_path.string(),".graph", ".sol");
+				shared_ptr<MISGNNData> mis = MISGNNData::load(graph_path.string(), feature_path.string(), solution_path.string());
+
+				mis->to(device, true);
+				rw_guard.lock();
+				mis->save_to_cache(cached_example_path);
+				rw_guard.unlock();
+				return Example(mis, mis->y);
+			}
 		}
 		Batch get_batch(BatchRequest request) {
 			return torch::data::datasets::Dataset<MISGNNDataset,Example>::get_batch(request);
@@ -350,7 +430,20 @@ struct MISGNNStack : public torch::data::transforms::Stack<> {
 
 		int x_n=0, edge_index_n=0, y_n=0, var_mask_n=0, var_marginal_mask_n=0,
 		    x_offset=0, edge_index_offset=0, y_offset=0, var_mask_offset=0, var_marginal_mask_offset=0, y_ind_n=0, y_ind_offset=0;
-		int x_dim = 0;
+
+		int r_edges_n=0, r_edges_offset=0;
+		int q_edges_n=0, q_edges_offset=0;
+		int f2v_edges_n=0, f2v_edges_offset=0;
+		int m_edges_n=0, m_edges_offset=0;
+		int vm_edges_n=0, vm_edges_offset=0;
+		int m_stride_n=0, m_stride_offset=0;
+		int ss_index_n=0, ss_index_offset=0;
+		int v_ss_mask_n=0, v_ss_mask_offset=0;
+
+		int total_ss=0;
+		int total_vars=0, total_fact=0;
+
+		int x_dim = 0, cc=0;
 		for(auto & ex : examples) {
 
 			x_n += ex.data->x.size(0);
@@ -360,6 +453,20 @@ struct MISGNNStack : public torch::data::transforms::Stack<> {
 			y_ind_n += ex.data->y_ind.size(0);
 			var_mask_n += ex.data->var_mask.size(0);
 			var_marginal_mask_n += ex.data->var_marginal_mask.size(0);
+
+			// FGEdges
+			r_edges_n += ex.data->bp_edges->r_edges.size(0);
+			q_edges_n += ex.data->bp_edges->q_edges.size(0);
+			f2v_edges_n += ex.data->bp_edges->f2v_edges.size(0);
+			total_ss += ex.data->bp_edges->num_ss.item<int>();
+			m_edges_n += ex.data->bp_edges->m_edges.size(0);
+			vm_edges_n += ex.data->bp_edges->vm_edges.size(0);
+			m_stride_n += ex.data->bp_edges->m_stride.size(0);
+			ss_index_n += ex.data->bp_edges->ss_index.size(0);
+			v_ss_mask_n += ex.data->bp_edges->v_ss_index_mask.size(0);
+			total_vars += ex.data->bp_edges->num_vars[0].item<int>();
+			total_fact += ex.data->bp_edges->num_fact[0].item<int>();
+			cc++;
 		}
 
 		auto l_opts = TensorOptions().dtype(kLong).device(device);
@@ -372,12 +479,44 @@ struct MISGNNStack : public torch::data::transforms::Stack<> {
 		       var_mask = torch::zeros({var_mask_n}, b_opts), 
 		       var_marginal_mask = torch::zeros({var_marginal_mask_n}, b_opts);
 
+		Tensor new_r_edges = torch::empty({r_edges_n,2}, l_opts);
+		Tensor new_q_edges = torch::empty({q_edges_n,2}, l_opts);
+		Tensor new_f2v_edges = torch::empty({f2v_edges_n,2}, l_opts);
+		Tensor new_m_edges = torch::empty({m_edges_n,2}, l_opts);
+		Tensor new_vm_edges = torch::empty({vm_edges_n,2}, l_opts);
+		Tensor new_num_vars = torch::empty({(long int)examples.size()}, l_opts);
+		Tensor new_num_ss = torch::empty({(long int)examples.size()}, l_opts);
+		Tensor new_num_fact = torch::empty({(long int)examples.size()}, l_opts);
+		Tensor new_f2v_msg = torch::empty({(long int)examples.size()}, l_opts);
+		Tensor new_m_stride = torch::empty({m_stride_n}, l_opts);
+		Tensor new_v_ss_mask = torch::empty({v_ss_mask_n}, b_opts);
+		Tensor new_ss_index = torch::empty({ss_index_n}, l_opts);
+
 		int c = 0, n = 0, m = 0;
 		uint64_t vertex_offset=0;
-		shared_ptr<struct FGEdges> bp_edges;
+		//shared_ptr<struct FGEdges> bp_edges;
 		shared_ptr<struct FactorGraph> fg;
 
+		int total_stack_t = 0;
+
+		//long int num_f2v_messages_1 = fg1->num_f2v_messages();
+		//long int num_f2v_subsum = edges1->f2v_edges.index({Slice(),0}).max().item<int>()+1;
+		Tensor r_offsets = tensor({0,0}, l_opts);
+		Tensor q_offsets = tensor({0,0}, l_opts);
+		Tensor f2v_less_offsets = tensor({0,0}, l_opts);
+		Tensor f2v_more_offsets = tensor({0,total_ss}, l_opts);
+		Tensor m_offsets = tensor({0,0}, l_opts);
+		Tensor vm_offsets = tensor({0,0}, {device}).to(torch::kLong);
+		int ss_index_true_offset=0, ss_index_false_offset=2*total_vars;
+
+		int num_f2v_subsum, num_vars, num_fact, num_ss;
+
+
 		for(auto & ex : examples) {
+			num_f2v_subsum = ex.data->bp_edges->f2v_edges.index({Slice(),0}).max().item<int>()+1;
+			num_vars = ex.data->bp_edges->num_vars.item<int>();
+			num_fact = ex.data->bp_edges->num_fact.item<int>();
+			num_ss = ex.data->bp_edges->num_ss.item<int>();
 
 			x.index_put_({Slice(x_offset, x_offset+ex.data->x.size(0)), Slice()},ex.data->x.to(device, true));
 
@@ -400,28 +539,108 @@ struct MISGNNStack : public torch::data::transforms::Stack<> {
 			var_marginal_mask.index_put_({Slice(var_marginal_mask_offset, var_marginal_mask_offset+ex.data->var_marginal_mask.size(0))}, ex.data->var_marginal_mask.to(device,true));
 			var_marginal_mask_offset += ex.data->var_marginal_mask.size(0);
 
+			new_r_edges.index_put_({Slice(r_edges_offset, r_edges_offset+ex.data->bp_edges->r_edges.size(0)), Slice()}, 
+						ex.data->bp_edges->r_edges + r_offsets);
+			r_offsets[0] += ex.data->bp_edges->num_f2v_msg[0];
+			r_offsets[1] += num_f2v_subsum;//ex.data->bp_edges->f2v_edges.index({Slice(),0}).max().item<int>()+1;
+			r_edges_offset += ex.data->bp_edges->r_edges.size(0);
+
+			new_q_edges.index_put_({Slice(q_edges_offset, q_edges_offset+ex.data->bp_edges->q_edges.size(0)), Slice()}, 
+						ex.data->bp_edges->q_edges + q_offsets);
+			q_offsets += ex.data->bp_edges->num_f2v_msg[0];
+			q_edges_offset += ex.data->bp_edges->q_edges.size(0);
+
+			Tensor tmp_f2v_edges = ex.data->bp_edges->f2v_edges.clone();
+			Tensor idx_less = (tmp_f2v_edges.index({Slice(),1}) < ex.data->bp_edges->num_ss[0]);
+			Tensor idx_more = (tmp_f2v_edges.index({Slice(),1}) >= ex.data->bp_edges->num_ss[0] );
+			tmp_f2v_edges.index_put_({idx_less,Slice()}, tmp_f2v_edges.index({idx_less,Slice()}) + f2v_less_offsets);
+			tmp_f2v_edges.index_put_({idx_more,Slice()}, tmp_f2v_edges.index({idx_more,Slice()}) + f2v_more_offsets - tensor({0,num_ss}, l_opts));
+
+			new_f2v_edges.index_put_({Slice(f2v_edges_offset, f2v_edges_offset+ex.data->bp_edges->f2v_edges.size(0)), Slice()}, 
+						tmp_f2v_edges);
+			f2v_less_offsets[0] += num_f2v_subsum;
+			f2v_less_offsets[1] += ex.data->bp_edges->num_ss[0];
+			f2v_more_offsets[0] += num_f2v_subsum;
+			f2v_more_offsets[1] += ex.data->bp_edges->num_f2v_msg[0];
+
+			f2v_edges_offset += ex.data->bp_edges->f2v_edges.size(0);
+
+
+			new_m_edges.index_put_({Slice(m_edges_offset, m_edges_offset+ex.data->bp_edges->m_edges.size(0)), Slice()}, 
+						ex.data->bp_edges->m_edges + m_offsets);
+			m_offsets[0] += ex.data->bp_edges->num_ss[0];
+			m_offsets[1] += ex.data->bp_edges->num_f2v_msg[0];
+			m_edges_offset += ex.data->bp_edges->m_edges.size(0);
+
+			new_vm_edges.index_put_({Slice(vm_edges_offset, vm_edges_offset+ex.data->bp_edges->vm_edges.size(0)), Slice()}, 
+						ex.data->bp_edges->vm_edges + vm_offsets);
+			vm_offsets[0] += 2*ex.data->bp_edges->num_vars[0];
+			vm_offsets[1] += ex.data->bp_edges->num_f2v_msg[0];
+			vm_edges_offset += ex.data->bp_edges->vm_edges.size(0);
+
+			new_num_vars[c] = ex.data->bp_edges->num_vars[0];
+			new_num_ss[c] = ex.data->bp_edges->num_ss[0];
+			new_num_fact[c] = ex.data->bp_edges->num_fact[0];
+			new_f2v_msg[c] = ex.data->bp_edges->num_f2v_msg[0];
+
+			new_m_stride.index_put_({Slice(m_stride_offset, m_stride_offset+ex.data->bp_edges->m_stride.size(0))}, 
+						ex.data->bp_edges->m_stride);
+			m_stride_offset += ex.data->bp_edges->m_stride.size(0);
+
+			new_v_ss_mask.index_put_({Slice(v_ss_mask_offset, v_ss_mask_offset+ex.data->bp_edges->v_ss_index_mask.size(0))}, 
+						ex.data->bp_edges->v_ss_index_mask);
+			v_ss_mask_offset += ex.data->bp_edges->v_ss_index_mask.size(0);
+
+
+			Tensor tmp_ss_index = ex.data->bp_edges->ss_index.clone();
+			tmp_ss_index.index_put_({ex.data->bp_edges->v_ss_index_mask}, tmp_ss_index.index({ex.data->bp_edges->v_ss_index_mask}) + ss_index_true_offset);
+			tmp_ss_index.index_put_({ex.data->bp_edges->v_ss_index_mask.logical_not()}, tmp_ss_index.index({ex.data->bp_edges->v_ss_index_mask.logical_not()}) + ss_index_false_offset - 2*num_vars);
+
+			ss_index_true_offset += 2*num_vars;
+			ss_index_false_offset += 4*(num_fact - num_vars);
+			new_ss_index.index_put_({Slice(ss_index_offset, ss_index_offset+ex.data->bp_edges->ss_index.size(0))}, 
+						tmp_ss_index);
+			ss_index_offset += tmp_ss_index.size(0);
 
 
 
-			if(c==0) {
-				bp_edges = copy(ex.data->bp_edges);
-				fg = copy(ex.data->fg);
+			//auto stack_start = high_resolution_clock::now();
+			//if(c==0) {
+			//	bp_edges = copy(ex.data->bp_edges);
+			//	fg = copy(ex.data->fg);
 
-			} else {
-				stack_bpedges(bp_edges, ex.data->bp_edges, fg, ex.data->fg);
-				stack_fg(fg, ex.data->fg);
+			//} else {
+			//	stack_bpedges(bp_edges, ex.data->bp_edges, fg, ex.data->fg);
+			//	stack_fg(fg, ex.data->fg);
 
-			}
+			//}
+			//auto stack_stop = high_resolution_clock::now();
+			//total_stack_t += duration_cast<microseconds>(stack_stop-stack_start).count();
 
-			n += ex.data->n;
-			m += ex.data->m;
-			vertex_offset += ex.data->edge_index.max().item<int>()+1;
+			n += num_vars;
+			m += num_fact - num_vars;
+			vertex_offset += num_fact;
 			c++;
 
 		}
 
 
 		shared_ptr<MISGNNData> ndata = std::make_shared<MISGNNData>();
+
+		//shared_ptr<FGEdges> bp_edges;
+		ndata->bp_edges->r_edges = new_r_edges;
+		ndata->bp_edges->q_edges = new_q_edges;
+		ndata->bp_edges->f2v_edges = new_f2v_edges;
+		ndata->bp_edges->m_edges = new_m_edges;
+		ndata->bp_edges->vm_edges = new_vm_edges;
+		ndata->bp_edges->num_vars = new_num_vars;
+		ndata->bp_edges->num_ss = new_num_ss;
+		ndata->bp_edges->num_fact = new_num_fact;
+		ndata->bp_edges->num_f2v_msg = new_f2v_msg;
+		ndata->bp_edges->m_stride = new_m_stride;
+		ndata->bp_edges->ss_index = new_ss_index;
+		ndata->bp_edges->v_ss_index_mask = new_v_ss_mask;
+
 
 		ndata->edge_index = edge_index;
 		ndata->x = x;
@@ -432,16 +651,20 @@ struct MISGNNStack : public torch::data::transforms::Stack<> {
 		ndata->n = n;
 		ndata->m = m;
 		ndata->fg = fg;
-		ndata->bp_edges = bp_edges;
+		//ndata->bp_edges = bp_edges;
 		ndata->n_batch = examples.size();
 
-		auto stop = high_resolution_clock::now();
-		auto duration = duration_cast<microseconds>(stop - batch_start);
-		//cout << "thread : " << std::this_thread::get_id() << " apply batch " << duration.count() << " bs " << examples.size() << endl;
 
 		ndata->to(device, true);
 
+		//auto stop = high_resolution_clock::now();
+		//auto duration = duration_cast<microseconds>(stop - batch_start);
+		//cout << "thread : " << std::this_thread::get_id() << " apply batch " << duration.count() << " stack " << total_stack_t << " bs " << examples.size() << endl;
 
+		//if(!torch::all(new_ss_index== bp_edges->ss_index).item<bool>()) {
+		//	cout << "New m_stride" << endl;
+		//	cout << torch::stack({new_ss_index, bp_edges->ss_index}, -1) << endl;
+		//}
 		return MISGNNExample(ndata, ndata->y);
 	};
 
@@ -485,20 +708,27 @@ struct FactorThetaPredictor : torch::nn::Module {
 	torch::nn::Linear fc1{nullptr}, fc2{nullptr};
 };
 
-vector<std::pair<shared_ptr<MISGNNData>,Tensor> > load_dataset(path dataset_path, torch::Device device) {
-	vector<std::pair<shared_ptr<MISGNNData>,Tensor> > dataset;
+vector<path> load_dataset(path dataset_path, torch::Device device) {
+	vector<path> dataset;
 	for(const auto& entry : directory_iterator(dataset_path)) {
 		if(endswith(entry.path().string(), ".graph")) {
-			path graph_path = entry.path();
-			path feature_path = replace_all(graph_path.string(),".graph", "_features.csv");
-			path solution_path = replace_all(graph_path.string(),".graph", ".sol");
-			shared_ptr<MISGNNData> mis = MISGNNData::load(graph_path.string(), feature_path.string(), solution_path.string());
-
-			mis->to(device, true);
-			dataset.push_back(std::pair<shared_ptr<MISGNNData>,Tensor>(mis,mis->y));
-
+			dataset.push_back(entry.path());
 		}
 	}
+
+	//vector<std::pair<shared_ptr<MISGNNData>,Tensor> > dataset;
+	//for(const auto& entry : directory_iterator(dataset_path)) {
+	//	if(endswith(entry.path().string(), ".graph")) {
+	//		path graph_path = entry.path();
+	//		path feature_path = replace_all(graph_path.string(),".graph", "_features.csv");
+	//		path solution_path = replace_all(graph_path.string(),".graph", ".sol");
+	//		shared_ptr<MISGNNData> mis = MISGNNData::load(graph_path.string(), feature_path.string(), solution_path.string());
+
+	//		mis->to(device, true);
+	//		dataset.push_back(std::pair<shared_ptr<MISGNNData>,Tensor>(mis,mis->y));
+
+	//	}
+	//}
 	return dataset;
 
 };
@@ -586,11 +816,13 @@ int main(int argc, char ** argv) {
 
 	path checkpoint_dir = output_dir / "checkpoints";
 	path loss_file_path = output_dir / "loss.csv";
+	path cache_dir = temp_directory_path() / output_dir.filename();
 
 	// Training summary
 	cout << "Summary" << endl;
 	cout << "\tDataset path    : " << dataset_path << endl;
 	cout << "\tCheckpoint dir  : " << checkpoint_dir << endl;
+	cout << "\tCache dir       : " << cache_dir << endl;
 	cout << "\tLoss file path  : " << loss_file_path << endl;
 	cout << "\tLearning rate   : " << lr << endl;
 	cout << "\tBatch size      : " << batch_size << endl;
@@ -627,20 +859,30 @@ int main(int argc, char ** argv) {
 	if(!exists(output_dir)) create_directories(output_dir);
 	create_directory(checkpoint_dir);
 
+	// create cache directory
+	if(!exists(cache_dir)) {
+		cout << "creating cache directory " << cache_dir << endl;
+		create_directory(cache_dir);
+	}
+
+
+
 	path train_dataset_path = dataset_path / "train";
 
 	// load datasets
-	vector<std::pair<shared_ptr<MISGNNData>,Tensor> > train_data;
+	vector<path> train_data, val_data;
 	cout << "Loading train data..." << flush;
 	train_data = load_dataset(train_dataset_path, device);
 	cout << "done" << endl;
 	// split dataset
 	size_t val_n = (size_t)(val_split * train_data.size());
-	vector<std::pair<shared_ptr<MISGNNData>,Tensor> > val_data(train_data.end() - val_n, train_data.end());
-	train_data = vector<std::pair<shared_ptr<MISGNNData>,Tensor> >(train_data.begin(), train_data.begin()+(train_data.size() - val_n));
+	val_data = vector<path>(train_data.end() - val_n, train_data.end());
+	train_data = vector<path>(train_data.begin(), train_data.begin()+(train_data.size() - val_n));
 	
 	//test_data = load_dataset(test_dataset_path, device);
-	auto train_set = MISGNNDataset(train_data).map(MISGNNStack());
+	auto train_set = MISGNNDataset(train_data, cache_dir, device).map(MISGNNStack());
+	auto val_set = MISGNNDataset(val_data, cache_dir, device).map(MISGNNStack());
+
 	cout << "Train data size     : " << train_data.size() << endl;
 	cout << "Validation data size: " << val_data.size() << endl;
 
@@ -650,9 +892,12 @@ int main(int argc, char ** argv) {
 			    	.batch_size(batch_size)
 				.workers(num_workers)
 			    );
-	//auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
-        //                    std::move(train_set), batch_size);
-
+	auto val_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+                            std::move(val_set), 
+			    torch::data::DataLoaderOptions()
+			    	.batch_size(1)
+				.workers(0)
+			    );
 
 	Tensor xx, pred, loss;
 	float best_val_loss = 1e14;
@@ -687,16 +932,16 @@ int main(int argc, char ** argv) {
 
  		torch::autograd::GradMode::set_enabled(false);
 		auto val_start = high_resolution_clock::now();
-		for(std::pair<shared_ptr<MISGNNData>,Tensor> & data : val_data) {
-			data.first->to(device, true);
-			xx = data.first->x.detach().clone();//.to(device);
+		for(auto batch: *val_loader) {
+			batch.data->to(device, true);
+			xx = batch.data->x.detach().clone();//.to(device);
 
 
-			pred = mnet->forward(xx, data.first->edge_index, *data.first);
+			pred = mnet->forward(xx, batch.data->edge_index, *batch.data);
 
 			loss = torch::binary_cross_entropy(
 					pred.clamp(0,1),
-					data.first->y_ind
+					batch.data->y_ind
 					);
 
 			epoch_val_losses.push_back(loss.item<float>());
